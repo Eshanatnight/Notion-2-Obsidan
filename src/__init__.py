e3f535e8959e55ed9f29a9b3498b9bc8d748b293
Eshan Chatterjee <eshan2001221@gmail.com>
2022-05-20 19:25:41 +0530

    Update 1

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+For those considering migrating content from [Notion](https://www.notion.so/)
+to [Obsidian](https://obsidian.md/), I've outlined detailed steps here that cover
+all the changes needed to make your export fully interlinked when opened as,
+or added to, an Obsidian vault.
+"""
